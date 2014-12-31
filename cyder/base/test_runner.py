84c393be4a28ba9fe2b02a210c5844bb59cd6909
drkitty <bumblingkitty@gmail.com>
2014-12-30 21:06:38 -0800

    Drop and recreate test database

--- cyder/base/test_runner.py
+++ cyder/base/test_runner.py
@@ -1,7 +1,9 @@
 from optparse import make_option
 
 import MySQLdb
-from django.db import connections
+from django.conf import settings
+from django.core.management import call_command
+from django.db import connection
 from django.test.simple import DjangoTestSuiteRunner
 
 from cyder.base.utils import get_cursor
@@ -20,30 +22,19 @@ class CorrectTestSuiteRunner(DjangoTestSuiteRunner):
         super(CorrectTestSuiteRunner, self).__init__(**options)
 
     def setup_databases(self, **kwargs):
-        """
-        Note: This function doesn't properly handle databases that are set as
-        test mirrors.
-        """
-        old_names = []
-        for connection in connections.all():
-            old_name = connection.settings_dict['NAME']
-            old_names.append(
-                (connection, old_name, True))
-            connection.settings_dict['NAME'] = 'test_' + old_name
-
+        connection.settings_dict['NAME'] = (
+            connection.creation._get_test_db_name())
         if self.recreate_db:
-            cur, db = get_cursor('default', use=False)
+            cur, name = get_cursor('default', use=False)
             try:
-                print 'Dropping test database...'
-                cur.execute('DROP DATABASE `{}`'.format(db))
+                cur.execute('DROP DATABASE `{}`'.format(name))
             except MySQLdb.OperationalError:
                 pass
-            print 'Creating test database...'
             cur.execute(
                 'CREATE DATABASE `{}` CHARACTER SET ascii '
-                'COLLATE ascii_general_ci'.format(db))
-
-        return old_names, []
+                'COLLATE ascii_general_ci'.format(name))
+                #connection.creation.create_test_db(autoclobber=True)
+            call_command('syncdb', interactive=False)
 
     def teardown_databases(self, *args, **kwargs):
         # Don't remove test databases.
