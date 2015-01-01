b1d5929670ce6995ee95907be16fa33aa4588f2e
drkitty <bumblingkitty@gmail.com>
2014-12-31 16:25:01 -0800

    temp

--- cyder/base/test_runner.py
+++ cyder/base/test_runner.py
@@ -1,6 +1,8 @@
 from optparse import make_option
+from sys import exit
 
 import MySQLdb
+import nose
 from django.conf import settings
 from django.core.management import call_command
 from django.db import connection
@@ -10,7 +12,11 @@ from cyder.base.utils import get_cursor
 
 
 class CorrectTestSuiteRunner(DjangoTestSuiteRunner):
-    option_list = (
+    option_list = tuple(filter(
+        lambda x: (
+            '-h' not in x._short_opts and '--version' not in x._long_opts),
+        nose.config.Config().getParser().option_list
+    )) + (
         make_option('-r', '--recreate-db',
                     action='store_true',
                     dest='recreate_db',
@@ -33,8 +39,17 @@ class CorrectTestSuiteRunner(DjangoTestSuiteRunner):
             cur.execute(
                 'CREATE DATABASE `{}` CHARACTER SET ascii '
                 'COLLATE ascii_general_ci'.format(name))
-                #connection.creation.create_test_db(autoclobber=True)
             call_command('syncdb', interactive=False)
+
+    def build_suite(self, test_labels, extra_tests=None, **kwargs):
+        return test_labels
+
+    def run_suite(self, suite, **kwargs):
+        print suite
+        class Nothing(object):
+            failures = ()
+            errors = ()
+        exit(int(not nose.run(*suite, **kwargs)))
 
     def teardown_databases(self, *args, **kwargs):
         # Don't remove test databases.
