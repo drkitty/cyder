594c8dae013b180c0f620687e8fbb3906841842e
drkitty <bumblingkitty@gmail.com>
2015-08-18 12:01:59 -0700

    Fix fail_mail

--- cyder/core/utils.py
+++ cyder/core/utils.py
@@ -48,37 +48,10 @@ def fail_mail(content, subject,
     msg['From'] = from_
     msg['To'] = ', '.join(to)
     s = smtplib.SMTP(settings.FAIL_EMAIL_SERVER, 587)
-    s.set_debuglevel(True)
     s.starttls()
-    s.ehlo()
+    s.login(settings.FAIL_EMAIL_FROM, settings.FAIL_EMAIL_PASSWORD)
     s.sendmail(from_, to, msg.as_string())
     s.quit()
-
-
-"""
-def mail_if_failure(msg, ignore=()):
-    def outer(func):
-        def inner(self, *args, **kwargs):
-            try:
-                return func(self, *args, **kwargs)
-            except ignore:
-                # Just let the exception through.
-                raise
-            except Exception:
-                # Send mail first.
-                error = msg + '\n' + format_exc_verbose()
-                self.log(syslog.LOG_ERR, error)
-                if not settings.TESTING:
-                    fail_mail(error, subject=msg)
-                with open(self.stop_file, 'w') as f:
-                    f.write(error)
-                raise
-        outer.__name__ = func.__name__
-        outer.__doc__ = func.__doc__
-        outer.__module__ = func.__module__
-        return inner
-    return outer
-"""
 
 
 class mail_if_failure(object):
